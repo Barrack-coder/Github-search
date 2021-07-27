465bddd532a9ab14fcc282c16dd50aa1712ff66d
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-07-27 14:18:06 +0300

    add clientsecret in in enviroment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
-};
+    production: false,
+    clientSecret: 'your client secret',
+    clientId: 'your client id'
+  };
 
 /*
  * For easier debugging in development mode, you can import the following file
