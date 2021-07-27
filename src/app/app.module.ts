9647618602717d1d69ed155a69a0956c4c11e63a
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-07-27 13:04:55 +0300

    Modified app.component.html,module.ts and create a new head file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
+import { HeadComponent } from './head/head.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    HeadComponent
   ],
   imports: [
     BrowserModule,
