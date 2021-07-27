3b41d693954eb8a7261b12927c20ce7ae8b69243
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-07-27 14:48:50 +0300

    updated data-count.pipe

--- src/app/date-count.pipes.spec.ts
+++ src/app/date-count.pipes.spec.ts
@@ -2,6 +2,7 @@ import { DateCount.Pipes } from './date-count.pipes';
 
 describe('DateCount.Pipes', () => {
   it('should create an instance', () => {
+     const pipe = new DateCountPipe();
     expect(new DateCount.Pipes()).toBeTruthy();
   });
 });
