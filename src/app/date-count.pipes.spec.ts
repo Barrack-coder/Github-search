53d7995b13ab84f9d1cc1c9e50a1034231e28178
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-07-27 14:09:34 +0300

    add three files date-count,repo and search-service ts

---
+++ src/app/date-count.pipes.spec.ts
@@ -0,0 +1,7 @@
+import { DateCount.Pipes } from './date-count.pipes';
+
+describe('DateCount.Pipes', () => {
+  it('should create an instance', () => {
+    expect(new DateCount.Pipes()).toBeTruthy();
+  });
+});
