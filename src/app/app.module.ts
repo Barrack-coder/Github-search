c24edc507827ea20dac3800f25e202807f345aa5
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-07-27 13:17:15 +0300

    created a new file error.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { HeadComponent } from './head/head.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
-    HeadComponent
+    HeadComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
